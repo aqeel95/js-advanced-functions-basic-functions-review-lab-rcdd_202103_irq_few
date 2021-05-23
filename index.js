b95281b342d750cc22417bb187c92d596328bf5d
Aqeel Aloraibi <aqeel.aloraibi@gmail.com>
2021-05-23 18:30:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+function saturdayFun(activity="roller-skate!"){
+  return `This Saturday, I want to ${activity}`
+}
+saturdayFun("bathe my dog!");
+
+function mondayWork(activity2="go to the office."){
+  return `This Monday, I will ${activity2}`
+}
+mondayWork("work from home.");
+
+
+function wrapAdjective(outer="*"){
+  
+  return function(inner="special"){
+    return`You are ${outer} ${inner}`}
+}
+
+wrapAdjective("ww")("vbd");
+
+//const encouragingPromptFunction = wrapAdjective("!!!")
+ 
+
+
+//"bathe my dog" "work from home"
